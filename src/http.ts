3b99660a3c21f4a204cb9a10541c0afc235a7d52
Davi Santos <daviir17@gmail.com>
2025-06-12 02:37:39 -0300

    tirando a barra do link permitido pelo cors

--- src/http.ts
+++ src/http.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://chat-web-eta-three.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://chat-web-eta-three.vercel.app'],
     methods: ['GET', 'POST'],
   })
 );
@@ -20,7 +20,7 @@ const port = 3000;
 
 const io = new Server(serverHttp, {
   cors: {
-    origin: ['http://localhost:5173', 'https://chat-web-eta-three.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://chat-web-eta-three.vercel.app'],
     methods: ['GET', 'POST'],
   },
 });
