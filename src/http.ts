503ef464ffb98ded7e6ece7f216375fcc0203c83
Davi Santos <daviir17@gmail.com>
2025-06-12 02:27:55 -0300

    alterando as configurações de cors

--- src/http.ts
+++ src/http.ts
@@ -7,17 +7,22 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://chat-web-eta-three.vercel.app/'],
+    methods: ['GET', 'POST'],
+  })
+);
 
 const serverHttp = http.createServer(app);
 
 const port = 3000;
 
 const io = new Server(serverHttp, {
-   cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST']
-  }
+  cors: {
+    origin: ['http://localhost:5173', 'https://chat-web-eta-three.vercel.app/'],
+    methods: ['GET', 'POST'],
+  },
 });
 
 export { serverHttp, io, port };
